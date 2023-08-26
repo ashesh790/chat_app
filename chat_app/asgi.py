845708db71881f5ad405f4115d435582407039c6
Ashesh790 <asheshtparmar@gmail.com>
2023-08-27 00:54:48 +0530

    first commit

---
+++ chat_app/asgi.py
@@ -0,0 +1,30 @@
+import os
+
+from django.core.asgi import get_asgi_application
+from django.urls import path
+
+from chat_1.consumers import ChatConsumer
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'API.settings')
+
+django_asgi_app = get_asgi_application()
+
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.routing import ProtocolTypeRouter, URLRouter, ChannelNameRouter
+
+application = ProtocolTypeRouter({
+    'http': django_asgi_app,
+    'websocket': AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                [
+                    path('', ChatConsumer.as_asgi()),
+                ]
+            )
+        )
+    ),
+    'channel': ChannelNameRouter({
+        'background': ChatConsumer.as_asgi()
+    })
+})
